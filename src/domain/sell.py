580d7c383df7c929add1f5f5d4cce258e0e96681
Matheus Moreira <matheusm920@gmail.com>
2021-12-05 11:35:23 -0300

    adding initial domains

---
+++ src/domain/sell.py
@@ -0,0 +1,7 @@
+class Sell:
+    def __init__(self, seller, customer, date, item_name, value):
+        self.seller = seller
+        self.customer = customer
+        self.date = date
+        self.item_name = item_name
+        self.value = value
